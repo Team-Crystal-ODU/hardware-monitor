f9092e573822e631fc3df308131149868b13d82d
hhawa01 <hhawa001@odu.edu>
2023-02-13 12:40:39 -0500

    adding code to ouput data to JSON file

---
+++ src/JsonOuput.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+class Ouput
+{
+    static void OutputDataAsJsonFile(object[] data, string fileName)
+    {
+        string json=JsonSerializer.Serialize(data);
+        File.WriteAllText(fileName, json);
+        Console.WriteLine("Data written to file: " + fileName);
+    }
+}
