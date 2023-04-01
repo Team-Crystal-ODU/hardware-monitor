dba66f6aaf44aad23e6b77f68cb32df8c08b0b6a
hhawa01 <hhawa001@odu.edu>
2023-04-01 13:24:37 -0400

    Removed code for json files, added code to push straight to API

--- hardware-monitor/src/Program.cs
+++ hardware-monitor/src/Program.cs
@@ -10,10 +10,7 @@ using System.Text.Json.Serialization;
 using System.IO;
 using System.Timers;
 using System.Net.Http.Json;
-
-
-
-
+using System.Net;
 
 internal sealed class Program
 { 
@@ -52,12 +49,7 @@ internal sealed class ConsoleHostedService : IHostedService
         };
 
         computer.Open();
-        System.Timers.Timer timer = new System.Timers.Timer(60000);
-        Console.WriteLine("Timer enabled at " + DateTime.Now); // Debug statement
-        timer.Elapsed += (source, e) => OnTimedEvent(Path.Combine(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "values"), "values.json"), timer);
-        timer.AutoReset = true;
-        timer.Enabled = true;
-        Console.WriteLine("Timer enabled successfully"); // Debug statement
+
 
         while (!cancellationToken.IsCancellationRequested)
         {
@@ -66,7 +58,7 @@ internal sealed class ConsoleHostedService : IHostedService
 
             Monitor(computer, Path.Combine(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "values"), "values.json"));
 
-            await Task.Delay(1000);
+            await Task.Delay(5000);
         }
 
         computer.Close();
@@ -83,30 +75,14 @@ internal sealed class ConsoleHostedService : IHostedService
     {
 
 
-        //computer.Accept(new UpdateVisitor());
+        computer.Accept(new UpdateVisitor());
 
 
 
         foreach (IHardware hardware in computer.Hardware)
         {
-           
-            // Console.WriteLine("Hardware: {0}", hardware.Name);
 
-            /* foreach (IHardware subhardware in hardware.SubHardware)
-             {
-                 Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
 
-                 foreach (ISensor sensor in subhardware.Sensors)
-                 {
-                     Console.WriteLine("Sensor: {0}, value: {1}", sensor.Name, sensor.Value);
-                 }
-             }
-            */
-            // if (File.Exists(file) && (DateTime.UtcNow - File.GetLastWriteTimeUtc(file)).TotalMinutes > maxFileAgeInMinutes)
-            // {
-            // Delete the file
-            //      File.Delete(file);
-            //   }
 
 
             foreach (ISensor sensor in hardware.Sensors)
@@ -114,12 +90,12 @@ internal sealed class ConsoleHostedService : IHostedService
                 string str = sensor.Name.Substring(0, 4);
                 DateTimeOffset now;
 
-                //string content;
-                //string content2 = "";
 
-                
+
                 //creating JSON object to store data in
                 object content = null;
+
+
 
 
                 //only grab power stats, ignore individual core temps
@@ -133,13 +109,12 @@ internal sealed class ConsoleHostedService : IHostedService
                     {
                         now = (DateTimeOffset)DateTime.UtcNow.ToLocalTime();
 
-                        //content = "\t\"cpu_watts\": "  + sensor.Value + ",";
-                        // content2 = "{\n\t\"timestamp\": " + now.ToString("yyyy-MM-ddTHH:mm:ss") + ",";
+                        
 
                         //creating json object to turn into json data later
                         content = new
                         {
-                            timestamp = now.ToString("yyyy-MM-ddTHH:mm:ss") + ",",
+                            timestamp = now.ToString("yyyy-MM-ddTHH:mm:ss"),
                             cpu_watts = sensor.Value
                         };
                     }
@@ -157,81 +132,35 @@ internal sealed class ConsoleHostedService : IHostedService
                     }
 
 
-                    //if file is empty, start w/ heading
-                    // if (new FileInfo(file).Length == 0)
-                    /*  if (!File.Exists(file))
-                      {                    
-                          using (StreamWriter writer = new StreamWriter(file))
-                          {
-                              // Write content to the file if necessary
-                              writer.WriteLine("{\"data\": [");
-                              if (content2 != "")
-                              {
-                                  writer.WriteLine(content2);
-                              }
-                              writer.WriteLine(content);
-                          }
-                      }
-
-                      //if file has content, append it
-                      else
-                      {
-                          using (StreamWriter writer = new StreamWriter(file, append: true))
-                          {
-                              if (content2 != "")
-                              {
-                                  writer.WriteLine(content2);
-                              }
-                              writer.WriteLine(content);
-                          }*/
 
 
-
-
-
-
+                    //Creating POST Request to API to send GPU and CPU wattage data 
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://172.18.12.16:6000/hardware");
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Method = "POST";
                     if (content != null)
                     {
-                        string jsonContent = JsonSerializer.Serialize(content);
-
-                        // Add the content to the file if it exists, or create a new file if it doesn't
-                        using (StreamWriter writer = File.Exists(file) ? File.AppendText(file) : File.CreateText(file))
+                        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                         {
-                            if (File.Exists(file) && writer.BaseStream.Length > 0)
-                            {
-                               //  Add a comma before the new object if the file already contains objects
-                                writer.WriteLine(",");
-                            }
+                            string jsonContent = JsonSerializer.Serialize(content);
+                            streamWriter.Write(jsonContent);
 
-                            writer.WriteLine(jsonContent);
+                        }
+
+                        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                        {
+                            var result = streamReader.ReadToEnd();
+                            Console.WriteLine(result);
                         }
                     }
 
-
-                    //if (File.Exists(file) && (DateTime.UtcNow - File.GetLastWriteTimeUtc(file)).TotalMinutes > maxFileAgeInMinutes)
-                    //{
-                   //     File.Delete(file);
-                  //  }
                 }
-              
-
             }
         }
-        computer.Accept(new UpdateVisitor());
+        
     }
-    private static void OnTimedEvent(string file, System.Timers.Timer timer)
-    {
-        Console.WriteLine("Deleting file at " + DateTime.Now); // Debug statement
 
-        // Check if the file exists and is too old
-        if (File.Exists(file))
-        {
-            // Delete the file
-            File.Delete(file);
-        }
-
-        Console.WriteLine("File deleted at " + DateTime.Now); // Debug statement
-    }
 
     public class UpdateVisitor : IVisitor
     {
